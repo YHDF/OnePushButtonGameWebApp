b0f5d4087b3ef2bc4e6555d0e4a16495e3d20098
kabba mohamed <kabba.kabba92@gmail.com>
2023-04-12 14:08:04 +0200

    front

--- onepushbuttonfront/src/App.js
+++ onepushbuttonfront/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import cara from './assets/cara.gif';
+import Mace from './assets/Mace.gif';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <div className = "App">
+        <header className = "App-header">
+        <div className="alley">
+            <div className="raven">
+            <img src={Mace} id="raven-pic"/>  
+            </div>
+            <center>
+                <div className="text-box"><a href="#slide1"> Welcome to One button Apple Adventure</a></div>
+            </center>
+            <div className="cara" onmouseover="moveGirl()"><img src={cara} id="cara-pic"/></div>
+        </div>
+        </header> 
+        </div>
+    );
 }
 
 export default App;
